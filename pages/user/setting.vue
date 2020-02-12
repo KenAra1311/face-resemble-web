22d79ecc263a66a9c59e38b35f075f7ad97306e7
KenAra <louis.9814@gmail.com>
2020-02-12 10:04:13 +0900

    ユーザネームとメールアドレスを変更できるよう改修

--- pages/user/setting.vue
+++ pages/user/setting.vue
@@ -1,24 +1,63 @@
 <template>
   <div v-if="user">
     <h1 class="text-align">アカウント情報</h1>
+    <p v-if="error" class="errors">{{error}}</p>
 
     <v-simple-table>
       <template v-slot:default>
         <tbody>
           <tr>
             <td>ユーザー名</td>
-            <td>{{ user.name }}</td>
+            <td>
+              <input
+                type="text"
+                v-model="user.name"
+                @click="clickUserName"
+                @keyup.enter="changeUserName"
+                @keypress="setCanSubmit"
+              >
+            </td>
+            <td>
+              <v-btn
+                :disabled="!pushUserNameChangeButton"
+                color="success"
+                @click="changeUserName"
+                small
+              >
+                変更
+              </v-btn>
+            </td>
           </tr>
           <tr>
             <td>メールアドレス</td>
-            <td>{{ user.email }}</td>
+            <td>
+              <input
+                type="text"
+                v-model="user.email"
+                @click="clickUserEmail"
+                @keyup.enter="changeUserEmail"
+                @keypress="setCanSubmit"
+              >
+            </td>
+            <td>
+              <v-btn
+                :disabled="!pushUserEmailChangeButton"
+                color="success"
+                @click="changeUserEmail"
+                small
+              >
+                変更
+              </v-btn>
+            </td>
           </tr>
           <tr>
             <td>投稿した顔写真の数</td>
             <td></td>
+            <td></td>
           </tr>
           <tr>
             <td>マッチングした顔写真の数</td>
+            <td></td>
             <td></td>
           </tr>
         </tbody>
@@ -46,9 +85,73 @@ export default {
     },
   },
 
-  data: () => ({}),
+  data: () => ({
+    canSubmit: false,
+    pushUserNameChangeButton: false,
+    pushUserEmailChangeButton: false,
+    error: '',
+  }),
 
   methods: {
+    setCanSubmit() {
+      this.canSubmit = true
+    },
+    clickUserName () {
+      this.pushUserNameChangeButton = true
+    },
+    clickUserEmail () {
+      this.pushUserEmailChangeButton = true
+    },
+    changeUserName () {
+      if ( !this.canSubmit || !this.user.name ) return
+      // 更新の確認
+      if ( !confirm('ユーザネームを変更しますか？') ) return
+
+      axios.put(`/v1/users/${this.user.id}`, { name: this.user.name })
+      .then(() => {
+        this.$store.commit('setNotice', {
+          status: true,
+          message: 'ユーザネームを変更しました',
+          type: 'success',
+        })
+        setTimeout(() => {
+          this.$store.commit('setNotice', {})
+        }, 2000)
+      })
+    },
+    changeUserEmail () {
+      if ( !this.canSubmit || !this.user.email ) return
+      // 更新の確認
+      if ( !confirm('メールアドレスを変更しますか？') ) return
+
+      this.$store.commit('setLoading', true)
+      firebase.auth().currentUser.updateEmail(this.user.email)
+      .then(() => {
+        axios.put(`/v1/users/${this.user.id}`, { email: this.user.email })
+        .then(() => {
+          this.$store.commit('setLoading', false)
+          this.$store.commit('setNotice', {
+            status: true,
+            message: 'メールアドレスを変更しました',
+            type: 'success',
+          })
+          setTimeout(() => {
+            this.$store.commit('setNotice', {})
+          }, 2000)
+        })
+      })
+      .catch(error => {
+        this.$store.commit('setLoading', false)
+        this.error = (code => {
+          switch ( code ) {
+            case 'auth/email-already-in-use':
+              return '※すでにそのメールアドレスは使用されています。'
+            default:
+              return '※サインアップから時間が経っているので、再度サインアップしてください。'
+          }
+        })(error.code)
+      })
+    },
     signOut () {
       if ( !confirm('サインアウトしますか？') ) return
 
@@ -77,4 +180,8 @@ export default {
 </script>
 
 <style scoped>
+.errors {
+  margin: 10px;
+  color: red;
+}
 </style>
