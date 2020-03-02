09ccc00e556cd56d9719459b6370988e7a8864bd
KenAra <louis.9814@gmail.com>
2020-03-02 16:44:53 +0900

    フォロページを「List.vue」で処理させるように改修

--- pages/follow/_id.vue
+++ pages/follow/_id.vue
@@ -1,31 +1,10 @@
 <template>
   <div v-if="user">
     <div v-if="followData[0]">
-      <h2 v-if="user.id === userData[0].id" class="text-center my-8">自分がフォローした人たち</h2>
-      <h2 v-else class="text-center my-8">{{ userData[0].name }} さんがフォローした人たち</h2>
-
-      <v-list class="mx-auto" max-width="344">
-        <v-list-item
-          v-for="(follow, index) in follows"
-          :key="index"
-          :to="'/user/' + follow.id"
-        >
-          <v-list-item-avatar>
-            <v-img
-              v-if="follow.profile_image"
-              :src="follow.profile_image"
-            ></v-img>
-            <v-icon v-else>
-              mdi-account-circle
-            </v-icon>
-          </v-list-item-avatar>
-
-          <v-list-item-content>
-            <v-list-item-title v-text="follow.name"></v-list-item-title>
-          </v-list-item-content>
-
-        </v-list-item>
-      </v-list>
+      <List
+        :listHeader="userData[0].name ? userData[0].name + 'さんがフォローした人たち' : '自分がフォローした人たち'"
+        :lists="follows"
+      ></List>
     </div>
 
     <div v-else>
@@ -60,11 +39,12 @@ export default {
       for ( var data of res.data ) {
         for ( var follow of this.followData ) {
           if ( data.id === follow.follow_id ) {
-            this.follows.push(data)
+            this.follows[0].user.push(data)
             continue
           }
         }
       }
+      this.follows[0].user = this.follows[0].user[0]
     })
     .catch(error => {
       console.log(error)
@@ -72,7 +52,9 @@ export default {
   },
 
   data: () => ({
-    follows: [],
+    follows: [
+      { user: [] },
+    ],
   }),
 
   fetch ({ store, redirect }) {
